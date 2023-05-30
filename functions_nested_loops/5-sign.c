eb8a0d1bd3f6994df7d5c200efe3cf3775263a5b
ericpo1sh <ericpo1sh@gmail.com>
2023-05-30 07:42:16 -0700

    tesr

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,27 +1,29 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_sign - checks n's value and prints out stats
- * n - has needs value checked.
+ *
+ * @n: needs value checked.
+ *
  * Return: 1 for n>0, 0 for n==0, -1 for n<0.
  */
 
 int print_sign(int n)
-
 {
 	if (n > 0)
 	{
 	_putchar(43);
-	return(1);
+	return (1);
 	}
 	else if (n == 0)
-	{	
+	{
 	_putchar(45);
-	return(-1);
+	return (-1);
 	}
 	else if (n < 0)
 	{
 	_putchar(48);
-	return(0);
+	return (0);
 	}
 }
