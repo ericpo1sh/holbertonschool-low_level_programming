2be6bb8dd5a8832d6a93ba9d82229a8e4809f121
ericpo1sh <ericpo1sh@gmail.com>
2023-05-30 07:50:28 -0700

    fttkycfvuykfygukyf

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -13,17 +13,14 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-	_putchar(43);
-	return (1);
+	return (0);
 	}
 	else if (n < 0)
 	{
-	_putchar(48);
-	return (0);
-	}
-	else (n = 0)
-	{
-	_putchar(45);
 	return (-1);
+	}
+	else (n == 0)
+	{
+	return (0);
 	}
 }
