46f1cf651d8a218406173c73e8702cd027fd9b91
ericpo1sh <ericpo1sh@gmail.com>
2023-05-29 21:37:33 -0700

    Write a function that computes the absolute value of an integer.

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -10,23 +10,20 @@
 int print_sign(int n)
 
 {
-		if (n > 0)
+	if (n > 0)
 	{
-		_putchar(43);
-
-		return(1);
+	_putchar(43);
+	return(1);
 	}
-		if (n == 0)
+	if (n == 0)
 	{	
-		_putchar(45);
-
-		return(0);
+	_putchar(45);
+	return(-1);
 	}
-		if (n < 0)
+	if (n < 0)
 	{
-		_putchar(48);
-
-		return(-1);
+	_putchar(48);
+	return(0);
 	}
 
 }
