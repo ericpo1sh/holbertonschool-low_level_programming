5413507ca85c9b1dfbd8a9ffceb5ac88085e0ab8
ericpo1sh <ericpo1sh@gmail.com>
2023-05-31 06:52:47 -0700

    dasd

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - adds two variables together.
+ * @a - number that holds value
+ * @b - number that holds other value
+ * result - result of two int's
+ * Return: prints result.
+ * int : number that will be chosen and added
+ */
+
+int add(int, int)
+{
+	int a;
+	int b;
+
+	result = a + b
+
+	return (result);
+}
