78ae419c9e2ae8fdd02a7c7aaf94e145589f1a53
ericpo1sh <ericpo1sh@gmail.com>
2023-06-01 10:36:14 -0700

    Write a function that draws a diagonal line on the terminal.

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagonal - will print a backslash in a cool diagnal line.
+ * @n : how many lines, length line wise
+ * space adds to the ammount of spaces, slash adds a slash when equal to n.
+ */
+void print_diagonal(int n)
+{
+	int space;
+	int slash;
+
+	if (n < 0)
+	{
+		for (slash = 0; slash < n; slash++)
+	{
+		for (space = 0; space < n; space++)
+			if (slash == n)
+				_putchar('\\');
+			else
+				_putchar(' ');
+	}
+		_putchar('\n');
+}
+}
