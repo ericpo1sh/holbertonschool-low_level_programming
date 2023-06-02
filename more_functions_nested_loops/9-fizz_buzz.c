b282807f06b882822af450f60f5b99eaa4f37445
ericpo1sh <ericpo1sh@gmail.com>
2023-06-01 18:10:57 -0700

    not done yet

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *
+ *
+ *
+ */
+
+int main(void)
+{
+	int count;
+	int fizz = (count % 3);
+	int buzz = (count % 5);
+
+	for (count = 1; count < 100; count++)
+	{
+		if (
+		
