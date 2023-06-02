bc5a6b07c9cb2823ccb4968c825914ea86dd21a7
ericpo1sh <ericpo1sh@gmail.com>
2023-06-02 08:15:34 -0700

    The “Fizz-Buzz test”

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,17 +1,32 @@
 #include <stdio.h>
 /**
- *
- *
- *
+ * main - will count 1-100 and print fizzbuzz, fizz, buzz, when conditions meet
+ * Return: i dont have one.
+ * cnt is current number that will print
  */
 
 int main(void)
 {
-	int count;
-	int fizz = (count % 3);
-	int buzz = (count % 5);
+	int cnt;
 
-	for (count = 1; count < 100; count++)
-	{
-		if (
-		
+	cnt = 1;
+
+	for (cnt == 1; cnt < 100; cnt++)
+		{
+		if ((cnt % 5 == 0) && (cnt % 3 == 0))
+		{
+			printf("FizzBuzz ");
+			putchar(' ');
+		}
+		if (cnt % 5 == 0)
+		{
+			printf("Fizz ");
+		}
+		if (cnt % 3 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+			printf("%d ", cnt);
+	}
+}
