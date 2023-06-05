dff7702d8f85d6c49afb401eef4bc296c41ac7a5
ericpo1sh <ericpo1sh@gmail.com>
2023-06-05 11:32:55 -0700

    Write a function that prints n elements of an array of integers, followed by a new line.

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * print_array - will print out random arrays given
+ *
+ * @a : the var pointing to the array
+ * @n : is the array number that will be random.
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		printf("%d", a[i]);
+
+	if (i == n - 1)
+	continue;
+
+	printf(", ");
+	}
+	printf("\n");
+}
