a3ed868de26bde6e9e8414facc2b42cb5bd3f8ea
ericpo1sh <ericpo1sh@gmail.com>
2023-06-07 09:55:22 -0700

    test

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ *
+ *
+ *
+ *
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int x < n;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	while (src[x] != '\0')
+	{
+		i++;
+		x++;
+	}
+	return (dest);
+}
