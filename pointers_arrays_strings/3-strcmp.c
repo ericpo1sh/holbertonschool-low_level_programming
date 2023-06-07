dc60182f33a2858f1a2948fd7b59dbeab2b4ad9c
ericpo1sh <ericpo1sh@gmail.com>
2023-06-07 12:55:26 -0700

    test

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ *
+ *
+ *
+ *
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	for
---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.c"
+/**
+ *
+ *
+ *
+ *
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; a[i] < a[n]; --n)
+}
