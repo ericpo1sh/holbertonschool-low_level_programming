bbf9e1be7798261a01f035dbda9bb1c5945659c0
ericpo1sh <ericpo1sh@gmail.com>
2023-06-08 10:13:04 -0700

    test

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -14,6 +14,7 @@ char *cap_string(char *s1)
 	while (s1[i])
 	{
 		for (s1[i] >= 'a' && s1[i] <= 'z'; i++)
+		{
 
 	if (s1[i] - 1 == ' ' ||
 	s1[i] - 1 == '\t' ||
@@ -29,7 +30,8 @@ char *cap_string(char *s1)
 	s1[i] - 1 == '{' ||
 	s1[i] - 1 == '}')
 		i == 0;
-	str[i] = (s1[i] - 32)
+	str[i] = (s1[i] - 32);
+		}
 	}
 	return (s1);
 }
