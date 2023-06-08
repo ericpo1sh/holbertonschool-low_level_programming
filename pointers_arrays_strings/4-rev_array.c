f004de8a9041870cb7472962f019b3cacdde70bb
ericpo1sh <ericpo1sh@gmail.com>
2023-06-07 19:08:34 -0700

    test

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -10,10 +10,11 @@
 void reverse_array(int *a, int n)
 {
 	int j;
+	int temp;
 
 	for (int i = 0; j = n - 1; i < j; i++, j--)
 	{
-		int temp = a[i];
+		temp = a[i];
 		a[i] = a[j];
 		a[j] = temp;
 	}
