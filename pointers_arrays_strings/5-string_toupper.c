0ea36e9239788cad37fcc2e17ec12922b217045d
ericpo1sh <ericpo1sh@gmail.com>
2023-06-08 08:22:36 -0700

    test

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -11,13 +11,12 @@
 char *string_toupper(char *s1)
 {
 	char i; 
-	int n = 0;
+	int n;
 
-	for (i = 'A'; i < 'Z'; i++)
+	for (n = 0; s1[n] != '\0'; n++)
 	{
-	if (s1[n] != i)
+	if (s1[n] > 'a' && s1[n] <= 'z')
 		s1[n] = (s1[n] - 32);
-	i = 'A';
 	}
 	return (s1);
 }
