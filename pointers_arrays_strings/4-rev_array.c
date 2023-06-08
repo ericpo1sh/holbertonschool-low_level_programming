069ff1ebae7e22f84f10b5bb3cfe668a967f69c4
ericpo1sh <ericpo1sh@gmail.com>
2023-06-07 20:29:43 -0700

    Write a function that reverses the content of an array of integers.

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -2,10 +2,12 @@
 #include <string.h>
 #include "main.h"
 /**
- *
- *
- *
- *
+ * reverse_array - will reverse the order of the array
+ * @a : this value holds the array
+ * @n : this value represents the size
+ * j is my decrementing array pointer
+ * i is my incrementing array pointer
+ * temp is the temporary int i use to swap orders
  */
 void reverse_array(int *a, int n)
 {
