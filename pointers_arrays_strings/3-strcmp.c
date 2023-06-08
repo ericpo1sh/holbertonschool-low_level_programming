3ff61312a55b75d95717b324286d54f1b06782d1
ericpo1sh <ericpo1sh@gmail.com>
2023-06-07 18:43:24 -0700

    test

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -9,6 +9,16 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
+	int flag = 0;
+	int i;
 
-	for
+	for (i = 0; s1 [i] != '\0' || s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			flag = 1;
+			break;
+		}
+	}
+	if (flag == 1)
+
