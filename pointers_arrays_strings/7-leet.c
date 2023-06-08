6d894f09bfdc069677091b4654aa7291a2de59dd
ericpo1sh <ericpo1sh@gmail.com>
2023-06-08 13:09:27 -0700

    test

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -9,4 +9,23 @@
  */
 char *leet(char *s)
 {
+	int i;
+	int j;
 
+	char a1[10] = "AEOTLaeotl"
+	char a2[10] = "4307143071"
+
+	while (s[i] != '\0')
+		for (i = 0; i < 10; i++)
+		{
+			for (j = 0; j < 10; j++)
+			{
+				if (s[i] == a1[i])
+				{
+				s[i] = a2[j];
+				}
+			}
+		}
+s[i] = '\0';
+return (s);
+}
