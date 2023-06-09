a777005142e93ef5bb406a584137d716eeb1de2f
ericpo1sh <ericpo1sh@gmail.com>
2023-06-09 12:48:21 -0700

    tesy

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -9,16 +9,12 @@
 char *_strbrk(char *s, char *accept)
 {
 	int i, j;
-	int z;
 
 	for (i = 0; s[i] != '\0'; i++)
 		for (j = 0; accept[j] != '\0'; j++)
 			if (s[i] == accept[j])
 			{
 				s[i] = accept[j];
-				z = 1;
 			}
-	if (z == 1)
-		return(s);
-	else
-		return(0);
+	return(s);
+}
