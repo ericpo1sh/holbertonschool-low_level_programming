3aa586445c21e6d68745af52096f5dbd25264e4d
ericpo1sh <ericpo1sh@gmail.com>
2023-06-09 09:36:55 -0700

    v

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -2,10 +2,20 @@
 #include <string.h>
 #include "main.h"
 /**
- *
- *
- *
+ * _memcpy - will copy everything from the src to dest
+ * @dest : our destination of what we're copying
+ * @src : the source of what we are copying
+ * @n : the size in bytes of how many chars we can cpy
+ * Return: dest.
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
 
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+
+	return (dest);
+
+}
