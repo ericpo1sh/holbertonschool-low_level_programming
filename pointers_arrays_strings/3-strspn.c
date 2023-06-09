95cb16a940e242191d7314e48850e30b3c123fc4
ericpo1sh <ericpo1sh@gmail.com>
2023-06-09 11:58:06 -0700

    test

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <string.h>
+/**
+ *
+ *
+ *
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i = 0;
+	int j = 0;
+	int cnt = 0;
+
+	for (; s[i] != '\0'; i++)
+	{
+		if (cnt != i)
+		{
+			for (; accept[j] != '\0'; j++)
+			{
+				if (s[i] == accept[j])
+				{
+					cnt++;
+				}
+			}
+		}
+	}
+	return (cnt);
+}
