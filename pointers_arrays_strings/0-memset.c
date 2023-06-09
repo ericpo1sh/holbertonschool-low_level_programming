b3ca6ba20d9860e84d826b40e82c9882ac40e3a4
ericpo1sh <ericpo1sh@gmail.com>
2023-06-09 09:15:56 -0700

    Write a function that fills memory with a constant byte.

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -2,10 +2,11 @@
 #include <stdio.h>
 #include <string.h>
 /**
- *
- *
- *
- *
+ * _memset - will convert an unsigned int to an unsigned char
+ * @s : the pointer thats pointing to the string that is updated
+ * @b : the char that unsigned int n will convert to
+ * @n : the size in bytes
+ * Return: the result (s)
  */
 
 char *_memset(char *s, char b, unsigned int n)
