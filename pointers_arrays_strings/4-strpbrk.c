f5131d559c53ed5eaeb3971e3f6ca351f08fb1ad
ericpo1sh <ericpo1sh@gmail.com>
2023-06-09 13:59:07 -0700

    test

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -10,8 +10,8 @@ char *_strpbrk(char *s, char *accept)
 {
 	int i, j;
 
-	for (i = 0; s[i] != '\0'; i++)
-		for (j = 0; accept[j] != '\0'; j++)
+	for (j = 0; accept[j] != '\0'; j++)
+		for (i = 0; s[i] != '\0'; i++)
 			if (s[i] == accept[j])
 			{
 				s[i] = accept[j];
