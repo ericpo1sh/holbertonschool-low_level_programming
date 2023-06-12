cf0f5eb8d87e4fc43d56755b75da27e0b910b621
ericpo1sh <ericpo1sh@gmail.com>
2023-06-12 12:52:10 -0700

    test

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ *
+ */
+char *_strstr(char *h, char *n)
+{
+	int i, j = 0;
+
+	if (n[j] = '\0')
+	return (&h[i]);
+
+	while (h[i] != '\0')
+		if (h[i] == n[j])
+		{
+			i++;
+			j++;
+		if (n[j] == '\0')
+			return (&h[i]);
+		}
+	return (NULL);
+}
