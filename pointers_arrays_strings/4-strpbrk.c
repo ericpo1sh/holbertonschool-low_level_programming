9301a0bf183edae84bf0d9dd89254e85afae9e90
ericpo1sh <ericpo1sh@gmail.com>
2023-06-12 08:33:40 -0700

    test

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -12,12 +12,11 @@ char *_strpbrk(char *s, char *accept)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; accept[j] != '\0';)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
 			{
 				return (&s[i]);
-				j++;
 			}
 		}
 	}
