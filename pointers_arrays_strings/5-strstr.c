eccfb3b18c68766d5ee7906505ff825fe27dccad
ericpo1sh <ericpo1sh@gmail.com>
2023-06-12 13:09:05 -0700

    test

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -10,6 +10,7 @@ char *_strstr(char *h, char *n)
 {
 	int i = 0;
 	int j = 0;
+	int g = 0;
 
 	if (n[j] == '\0')
 	{
@@ -19,10 +20,14 @@ char *_strstr(char *h, char *n)
 	while (h[i] != '\0')
 		if (h[i] == n[j])
 		{
+			if (g == 0)
 			i++;
 			j++;
-		if (n[j] == '\0')
+			if (n[j] == '\0')
 			return (&h[i]);
 		}
+	i = 0;
+	j = 0;
+	i++;
 	return (NULL);
 }
