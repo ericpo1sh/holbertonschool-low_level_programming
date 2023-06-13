49e9fc2fdffe78b699009e8b1b50ab357af5f118
ericpo1sh <ericpo1sh@gmail.com>
2023-06-12 18:18:51 -0700

    test

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -8,20 +8,13 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int c3[3][3] = {
-		{0, 1, 5},
-		{10, 11, 12},
-		{1000, 101, 102},
-	};
+	int i, n = 0;
+	int sum1, sum2 = 0;
 
-	int c5[5][5] = {
-		{0, 1, 5, 12124, 1234},
-		{10, 11, 12, 123521, 12512},
-		{1000, 101, 102, 12545, 214543435},
-		{100, 1012451, 11102, 12545, 214543435},
-		{10, 12401, 10452, 11542545, 1214543435},
-	};
-	print_diagsums((int *)c3, 3);
-	print_diagsums((int *)c5, 5);
-	return (0);
+	for (i = 0; i <= (size * size); i = i + size + 1)
+		sum1 = sum1 + a[i];
+
+	for (n = size - 1; n <= (size * size) - size; n = n + size - 1)
+		sum2 = sum2 + a[n];
+	printf("%d, %d\n", sum1, sum2);
 }
