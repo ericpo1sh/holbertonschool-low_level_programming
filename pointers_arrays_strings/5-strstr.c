90b350aafd05672b6162163f1c4d3cc4f52bc4ba
ericpo1sh <ericpo1sh@gmail.com>
2023-06-12 18:06:52 -0700

    Write a function that locates a substring.

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -2,8 +2,8 @@
 #include <stddef.h>
 /**
  * _strstr - will check for first occurence of word in string
- * @h : this is the haystack string
- * @n : needle string, contains one word
+ * @haystack : this is the haystack string
+ * @needle : needle string, contains one word
  * Return: Adress of h[g] if null, whole string if true, null if nothing
  */
 char *_strstr(char *haystack, char *needle)
