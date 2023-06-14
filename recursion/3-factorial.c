e954d50d4abb7720dbe79695e20ffde768d27886
ericpo1sh <ericpo1sh@gmail.com>
2023-06-14 09:02:30 -0700

    Write a function that returns the factorial of a given number.

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
- *
- *
+ * factorial - this print out the factorial of N
+ * @n : the imput number
+ * Return: -1 if n le 0, 1 if is 0, and factorial if its ge.
  */
 int factorial(int n)
 {
@@ -10,7 +11,7 @@ int factorial(int n)
 
 	if (n == 0)
 	return (1);
-	
+
 	if (n > 0)
 	{
 	return (n * factorial(n - 1));
