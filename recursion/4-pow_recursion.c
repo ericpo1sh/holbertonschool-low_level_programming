e1fe7ca9073dbd83c50097642d3cd63f14d85fe2
ericpo1sh <ericpo1sh@gmail.com>
2023-06-14 09:23:24 -0700

    test

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -5,11 +5,14 @@
  */
 int _pow_recursion(int x, int y)
 {
+	int i = 0;
+
 	if (y < 0)
 		return (-1);
-	if (y > 0)
+	if (i < y)
 	{
-	return (_pow_recursion(x ^ y));
+	i++
+	return (_pow_recursion(x * x));
 	}
 	return (0);
 }
