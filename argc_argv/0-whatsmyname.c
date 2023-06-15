b4efbe4bbb6f289276a6769c5540200496203c84
ericpo1sh <ericpo1sh@gmail.com>
2023-06-15 08:45:48 -0700

    Write a program that prints its name, followed by a new line.

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -3,6 +3,7 @@
  * main - will print out the name of the file
  * @argc : counts the number of arguements
  * @argv : displays each string of the vector
+ * Return: always 0.
  */
 int main(int argc, char *argv[])
 {
