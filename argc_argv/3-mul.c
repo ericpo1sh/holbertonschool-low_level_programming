f6b81197eadfcaec27e53f4c84e87984dfe99769
ericpo1sh <ericpo1sh@gmail.com>
2023-06-15 09:54:15 -0700

    Write a program that multiplies two numbers.

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - will print out each string
+ * main - will mulitply and print result of two nums.
  * @argc : counts the number of arguements
  * @argv : displays each string of the vector
- * Return: always 0.
+ * Return: 1 if only 1 number, 0 if it works.
  */
 int main(int argc, char *argv[])
 {
+	if (argc < 3)
+	{
+	printf("Error\n");
+	return (1);
+	}
+
 	int sum1;
 	int sum2;
 
-	(void)argc;
-
-	{
 		sum1 = atoi(argv[1]);
 		sum2 = atoi(argv[2]);
-	}
 	printf("%d\n", (sum1 * sum2));
 	return (0);
 }
