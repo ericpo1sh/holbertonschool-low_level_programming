6dc82685ddb41b615f8ae33caf0849dc0b89f85a
ericpo1sh <ericpo1sh@gmail.com>
2023-06-16 13:14:10 -0700

    making my first stalic library

---
+++ static_libraries/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * _strlen - gives us the length of the string
+ *
+ * @s : var pointed to where string is stored.
+ *
+ * Return: the size of the current string.
+ */
+int _strlen(char *s)
+{
+	int i = strlen(s);
+
+	return (i);
+}
