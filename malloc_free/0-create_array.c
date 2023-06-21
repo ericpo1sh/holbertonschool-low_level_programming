a00bdcf3ae662e4360696b417aba269741cb7836
ericpo1sh <ericpo1sh@gmail.com>
2023-06-21 10:51:05 -0700

    test

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -8,4 +8,17 @@
  */
 char *create_array(unsigned int size, char c)
 {
+	char *b;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+
+	b = malloc(sizeof(char) * size);
+
+	if (b == '\0')
+		return (NULL);
+
+	return (b);
+}
 
