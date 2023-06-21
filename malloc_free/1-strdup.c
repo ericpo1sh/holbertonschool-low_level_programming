be539130c7290ef70a040b24def49a206fe19377
ericpo1sh <ericpo1sh@gmail.com>
2023-06-21 12:54:07 -0700

    test

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ *
+ *
+ *
+ */
+char *_strdup(char *str)
+{
+	char *arr;
+	int len;
+	int i;
+
+	if (size == 0)
+		return (NULL);
+
+	len = strlen(str) + 1;
+	arr = malloc(sizeof(char) * size);
+
+	if (arr == 0)
+		return (NULL);
+
+	for (i = 0; i < len; i++)
+		arr[i] = str[i]
+
+	return (arr);
+}
+
