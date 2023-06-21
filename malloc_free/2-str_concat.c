5da1bfc30446083f8be8e41e6df3d89e6c54d123
ericpo1sh <ericpo1sh@gmail.com>
2023-06-21 13:51:47 -0700

    test

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -15,25 +15,30 @@ char *str_concat(char *s1, char *s2)
 	char *dest;
 
 	if (s1 == 0)
-		return (NULL);
+		s1 = "";
 
 	if (s2 == 0)
-		return (NULL);
+		s2 = "";
 
-	len1 = strlen(s1) + 1;
+	len1 = strlen(s1);
 	len2 = strlen(s2) + 1;
 
 	dest = malloc((len1 + len2) * sizeof(char));
 
+	if (dest == 0)
+		return (NULL);
+
 	for (i = 0; i < len1; i++)
 	{
-		dest[x] = s1[i];
 		x++;
+		dest[x] = s1[i];
 	}
 	for (j = 0; j < len2; j++)
 	{
-		dest[x] = s2[j];
 		x++;
+		dest[x] = s2[j];
 	}
+	dest[x] = '\0';
+
 	return (dest);
 }
