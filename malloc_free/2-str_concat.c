54d89ceb92aff6c68bf663c3ab8590c1f91d0b58
ericpo1sh <ericpo1sh@gmail.com>
2023-06-21 13:36:39 -0700

    test

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ *
+ *
+ *
+ *
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int len1, len2, i, j;
+	char *dest;
+
+	if (s1 == 0)
+		return (NULL);
+
+	if (s2 == 0)
+		return (NULL);
+
+	len1 = strlen(s1) + 1;
+	len2 = strlen(s2) + 1;
+
+	dest = malloc((len1 + len2) * sizeof(char));
+
+	for (i = 0; i < len1; i++)
+		dest = s1[i];
+	for (j = 0; j < len2; j++)
+		dest = s2[j];
+	return (dest);
+}
