c24de4ccbe7c837ff75d4bdc75d8ba53d807e3e8
ericpo1sh <ericpo1sh@gmail.com>
2023-06-22 10:27:59 -0700

    Write a function that returns a pointer to a 2 dimensional array of integers.

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -2,10 +2,17 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- *
- *
- *
+ * free_grid - will retrun a pointer to our grid
+ * @grid : our grid
+ * @height : the heighth of matrix
  */
 void free_grid(int **grid, int height)
 {
+	int x;
+	
+	if (grid != NULL);
+	for (x = height; x >= 0; x--)
+		free(grid[x]);
 
+	free(grid);
+}
