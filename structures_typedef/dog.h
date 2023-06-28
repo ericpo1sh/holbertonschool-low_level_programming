0a1466afcd97022eafe6735508240be5b12625c6
ericpo1sh <ericpo1sh@gmail.com>
2023-06-28 09:11:56 -0700

    test

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+ *
+ *
+ */
+struct dog
+{
+	char *name = "Poppy";
+	float age = 3.5;
+	char *owner = "Bob";
+};
+int main(void)
+{
+	struct dog dog;
+	return (0);
+}
