ce5615d590516715b0d282a48db61f2238f02175
ericpo1sh <ericpo1sh@gmail.com>
2023-06-29 09:50:18 -0700

    who let the dogs out?????

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -3,10 +3,14 @@
 #include "dog.h"
 /**
  * free_dog - WHO LET THE DOGS OUT
+ * @d : doggy
  */
 void free_dog(dog_t *d)
 {
-	free(d->name);
-	free(d->owner);
-	free(d);
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
 }
