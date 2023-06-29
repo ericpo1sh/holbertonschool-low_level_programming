1f3cd310946b04a9b7e322492126f09ab906283d
ericpo1sh <ericpo1sh@gmail.com>
2023-06-29 12:22:38 -0700

    saving

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,6 +1,6 @@
 #ifndef __function_pointers__
 #define __function_pointers__
 void print_name(char *name, void (*f)(char *));
-
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif
