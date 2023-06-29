e98192957c2004f8d7312436dcc5593065d27ad5
ericpo1sh <ericpo1sh@gmail.com>
2023-06-29 12:49:07 -0700

    test

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -5,4 +5,11 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
+	int x = 0;
 
+	if (action != NULL || array != NULL)
+	{
+		for (; x < size; x++)
+			action(array[x]);
+	}
+}
