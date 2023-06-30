afbacf759540c2ab84f2336d7b54f95c6f0e843d
ericpo1sh <ericpo1sh@gmail.com>
2023-06-30 13:02:11 -0700

    Write a function that prints strings, followed by a new line.

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+ * print_strings - prints all the numbers in the arg list
+ * @separator : points to sep string
+ * @n : size of the arg list
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i = 0;
+	char *j;
+	va_list args;
+
+	va_start(args, n);
+
+	for (; i < n; i++)
+	{
+		j = va_arg(args, int);
+		if (j == NULL)
+			printf("(nil)")
+		else
+			printf("%s", j);
+		if (i != n - 1 && separator != NULL)
+		printf("%s", separator);
+	}
+		va_end(args);
+		printf("\n");
+}
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -2,5 +2,6 @@
 #define __variadic_functions__
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
 
 #endif
