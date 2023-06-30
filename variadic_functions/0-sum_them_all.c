9de679e674fdaccd802a286205ec82a55e422c26
ericpo1sh <ericpo1sh@gmail.com>
2023-06-30 12:10:13 -0700

    test

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -10,7 +10,7 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i = 0;
 	int sum = 0;
 	va_list args;
-	int j = va_arg(args, unsigned int);
+	int j = va_arg(args, int);
 
 	va_start(args, n);
 
