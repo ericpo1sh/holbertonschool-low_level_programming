89c707f8154f1372cdce9b26660741c73401e190
ericpo1sh <ericpo1sh@gmail.com>
2023-07-01 14:28:43 -0700

    test

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -20,7 +20,7 @@ void print_all(const char * const format, ...)
 		switch (format[i])
 		{
 			case 'c':
-				printf("%c", va_arg(args, char);
+				printf("%c", (char) va_arg(args, char);
 				break;
 			case 'd':
 				printf("%d", va_arg(args, int);
