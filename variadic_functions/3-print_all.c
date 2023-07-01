7675107ded98ff6a8dfe506647f6ba435f5bebba
ericpo1sh <ericpo1sh@gmail.com>
2023-07-01 14:10:59 -0700

    test

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -7,4 +7,33 @@
  */
 void print_all(const char * const format, ...)
 {
-	
+	va_list	args;
+	char *string;
+	int i = 0;
+
+
+	va_start(args, format);
+	while (format != NULL && format[i] != '\0')
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(args, char);
+				break;
+			case 'd':
+				printf("%d", va_arg(args, int);
+				break;
+			case 'f':
+				printf("%f", va_args(args, float);
+				break;
+			case 's':
+				string = va_arg(args, char *);
+				if (string == NULL);
+				{
+					printf("(nil)");
+					break;
+				}
+				printf("%s", string);
+				break;
+		}
+
