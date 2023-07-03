81fe652fa436719b858c113f7cccef9a59ee4476
ericpo1sh <ericpo1sh@gmail.com>
2023-07-03 11:34:08 -0700

    s

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,2 +1,23 @@
-t
+#include "calc.h"
+#include <stdio.h>
+/**
+ *
+ *
+ *
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+	
+	if (s != '+' || s != '-' || s != '*' ||
+			s != '/' || s != '%')
+		return (NULL);
 
