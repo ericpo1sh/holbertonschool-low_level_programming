cfa6db960b54d78804f55f41077ec5adb5d0c1b4
ericpo1sh <ericpo1sh@gmail.com>
2023-07-03 13:33:40 -0700

    test

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -17,7 +17,7 @@ int (*get_op_func(char *s))(int, int)
 	int i = 0;
 
 	if (s != NULL)
-		while (ops[i].op != NULL && *(ops[i].op) == *s)
+		while (ops[i].op != NULL && *(ops[i].op) != *s)
 			i++;
 	return (ops[i].f);
 }
