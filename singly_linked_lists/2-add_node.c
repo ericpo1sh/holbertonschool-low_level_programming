fe6bf28349d461f221f451e7541f10b10869f5e1
ericpo1sh <ericpo1sh@gmail.com>
2023-07-12 12:40:45 -0700

    ium sdasd

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -12,18 +12,13 @@ list_t *add_node(list_t **head, const char *str)
 	if ((head != NULL) && (str != NULL))
 	{
 		if (store == NULL)
-		{
 			return (NULL);
-		}
-		else
-		{
 		store = malloc((sizeof(list_t));
 		store->str = strdup(str);
 		store->len = strlen(str);
 		store->next = *head;
 		*head = store;
 		return (store);
-		}
 	}
 return (0);
 }
