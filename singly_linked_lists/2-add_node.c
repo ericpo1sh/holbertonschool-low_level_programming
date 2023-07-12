85317651bfd38d1b0bc05d917cbcbd3017bac790
ericpo1sh <ericpo1sh@gmail.com>
2023-07-12 12:17:06 -0700

    test

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -14,12 +14,11 @@ list_t *add_node(list_t **head, const char *str)
 	new = malloc(sizeof(list_t));
 
 	if (new == NULL)
-		return (0);
+		return (NULL);
 	
 	new->str = strdup(str);
 	new->len = strlen(str);
 	new->next = *head;
-
 	*head = new;
 
 return (new);
