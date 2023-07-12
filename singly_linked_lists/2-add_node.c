26fe0eb9792aa0382c0365951b4efc876e87be19
ericpo1sh <ericpo1sh@gmail.com>
2023-07-12 12:32:30 -0700

    test

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -11,11 +11,14 @@ list_t *add_node(list_t **head, const char *str)
 
 	if ((head != NULL) && (str != NULL))
 	{
-	new->str = strdup(str);
-	new->len = strlen(str);
-	new->next = *head;
-	*head = new;
-	return (new);
+		if (str == NULL)
+		{
+		new->str = strdup(str);
+		new->len = strlen(str);
+		new->next = *head;
+		*head = new;
+		return (new);
+		}
 	}
 return (0);
 }
