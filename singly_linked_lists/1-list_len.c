3dcc8c5181eef737531e3e17723079a508ff5d4b
ericpo1sh <ericpo1sh@gmail.com>
2023-07-12 10:26:41 -0700

    test

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *
+ *
+ *
+ */
+size_t print_list(const list_t *h)
+{
+        int i = 0;
+        char *str1 = NULL;
+
+        if (h == NULL)
+                return (0);
+
+        while (h)
+        {
+                str1 = h->str;
+                if (str1)
+                {
+                }
+                else
+                {
+                }
+                i++;
+                h = h->next;
+        }
+return (i);
+}
