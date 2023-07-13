b43f15d4e935c7c2cfa06e647fbaf871be318a0f
ericpo1sh <ericpo1sh@gmail.com>
2023-07-13 12:15:57 -0700

    Write a function that converts a binary number to an unsigned int.

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,56 @@
+#include "main.h"
+/**
+ * binary_to_uint - converts a number from binary to dec form
+ * @b : the binary string
+ * Return: 0 if NULL, sum if sucess.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int i, j = 0;
+	int sum, slen = 0;
+
+	slen = strlen(b - 1);
+
+	if (b != NULL)
+	{
+		for (i = slen; i >= 0; i--)
+		{
+			for (j = slen; j >= 0; j--)
+			{
+				if (b[j] == 1)
+				{
+					sum += pow(2,i);
+				}
+				else if (b[j] == 0)
+				{
+					sum += 0;
+				}
+				else
+					return (0);
+			}
+		}
+	}
+	else
+		return (0);
+
+return (sum);
+}
+
+/**
+ * _pow_recursion - gives us solution of x^y
+ * @x : our first int
+ * @y : our second int
+ * Return: -1 if y le 0, 1 if y is 0, result if y ge 0
+ */
+int pow(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	if (y > 0)
+	{
+	return (x * _pow_recursion(x, y - 1));
+	}
+	return (0);
+}
