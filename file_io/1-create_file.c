f46f1dc77efe65686d82ef9da4044f74d8b7bdcc
ericpo1sh <ericpo1sh@gmail.com>
2023-07-14 15:27:57 -0700

    test

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
-	if (fd == NULL)
+	if (fd == -1)
 		return (-1);
 
 	if (text_content)
