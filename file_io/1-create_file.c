d4a2d78241625909eac516df532f8e402f5525e7
ericpo1sh <ericpo1sh@gmail.com>
2023-07-14 12:53:45 -0700

    Create a function that creates a file.

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -23,8 +23,9 @@ int create_file(const char *filename, char *text_content)
 		while (text_content[i])
 		{
 			i++;
-			rite = write(fd, text_content, i);
 		}
+
+		rite = write(fd, text_content, i);
 		if (rite == -1)
 		{
 			close(fd);
