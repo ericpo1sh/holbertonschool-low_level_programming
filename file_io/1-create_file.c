06c55a5c36860740afb076a6f505d07ac0101d8b
ericpo1sh <ericpo1sh@gmail.com>
2023-07-14 15:33:39 -0700

    test

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -11,19 +11,15 @@ int create_file(const char *filename, char *text_content)
 	int i, write_result = 0;
 
 	if (filename == NULL)
-	{
 		return (-1);
-	}
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
 
-	while (text_content[i])
-	{
+	while (text_content && text_content[i])
 		i++;
-	}
 	write_result = write(fd, text_content, i);
 
 	if (write_result == -1)
