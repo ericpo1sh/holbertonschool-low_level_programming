4079ba1cba3ebfed89a54ff10b08a6071c85c035
ericpo1sh <ericpo1sh@gmail.com>
2023-07-14 11:23:24 -0700

    Write a script that reads from a file and prints contets into stdout

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * read_textfile - reads a text file and copies txt into stdout
+ * @filename : the filename given to us
+ * @letters : the size of the buffer
+ * Return: 0 if failed, new if passed
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t red, new;
+	char *bufR;
+
+	if (filename == NULL)
+		return (0);
+	
+	fd = open(filename, 0_RDONLY)
+	
+	if (fd == -1)
+		return (0);
+
+	bufR = malloc(sizeof(char) * (letters));
+
+	if (bufR == NULL)
+		return (0);
+
+	red = read(fd, bufR, letters);
+	new = write(STDOUT_FILENO, BufR, red);
+
+	close(fd);
+	free(bufR);
+	
+	return(new);
+}
+
