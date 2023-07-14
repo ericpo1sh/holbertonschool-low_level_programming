381f7e6697782279182235175e1885c110ad9eaa
ericpo1sh <ericpo1sh@gmail.com>
2023-07-14 15:51:19 -0700

    test

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content == NULL)
 	{
-		fd = open(filename, O_CREAT | O_RDWR, 0600);
+		fd = open(filename, O_WRONLY | O_CREAT, 0600);
 		if (fd == -1)
 		{
 			return (-1);
@@ -23,7 +23,7 @@ int create_file(const char *filename, char *text_content)
 		return (1);
 	}
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
