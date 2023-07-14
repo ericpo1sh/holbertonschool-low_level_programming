5d80680a42da526d807a9f550ec11bdd44073131
ericpo1sh <ericpo1sh@gmail.com>
2023-07-14 14:51:04 -0700

    Create a function that creates a file.

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -7,10 +7,10 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
+	int fd = 0;
 	int i, rite = 0;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
@@ -21,9 +21,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content)
 	{
 		while (text_content[i])
-		{
 			i++;
-		}
 
 		rite = write(fd, text_content, i);
 		if (rite == -1)
