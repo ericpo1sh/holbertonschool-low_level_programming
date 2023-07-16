60eb045e34f80fbf1b63c4d8c003ce2223858283
ericpo1sh <ericpo1sh@gmail.com>
2023-07-15 20:58:15 -0700

    test

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  *
  *
@@ -12,7 +13,6 @@ int main(int argc, char *argv[])
 	int to1 = 0;
 	int read1 = 0;
 	int write1 = 0;
-	int close1, close2 = 0;
 	int close1, close2 = 0;
 	char buff[BUFSIZ];
 
