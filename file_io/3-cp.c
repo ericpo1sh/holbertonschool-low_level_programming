97dd73e73009c2cbdb5b018ca1a70b02882f0096
ericpo1sh <ericpo1sh@gmail.com>
2023-07-15 21:04:07 -0700

    Write a program that copies the content of a file to another file.

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,53 +1,42 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *
- *
- *
- *
- *
+ * main - copies the content of a file to another file.
+ * @argc : arguement count
+ * @argv : arguement vector 
+ * Return 0 if all is passed!
  */
 int main(int argc, char *argv[])
 {
-	int from1 = 0;
-	int to1 = 0;
-	int read1 = 0;
-	int write1 = 0;
-	int close1, close2 = 0;
+	int from1, to1, read1, write1, close1, close2 = 0;
 	char buff[BUFSIZ];
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "usage: cp from_file file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp from_file file_to\n");
 		exit(97);
 	}
-
 	from1 = open(argv[1], O_RDONLY);
 	if (from1 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
 	to1 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	read1 = read(from1, buff, BUFSIZ);
 	write1 = write(to1, buff, read1);
-	
 	if (to1 == -1 || write1 != read1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-
 	if (read1 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
 	close1 = close(from1);
 	close2 = close(to1);
-
 	if (close1 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from1);
