73bfc755e55cab1621836ba333f5c103a08f0120
ericpo1sh <ericpo1sh@gmail.com>
2023-07-17 07:33:00 -0700

    test

--- more_singly_linked_lists/2-add_nodeint.c
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -7,11 +7,11 @@
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	list_int_t *store;
+	listint_t *store;
 
-	if ((head != NULL) && (str != NULL))
+	if ((head != NULL) && (n != NULL))
 	{
-		store = malloc((sizeof(list_int_t)));
+		store = malloc((sizeof(listint_t)));
 		if (store == NULL)
 			return (NULL);
 		store->n = n;
