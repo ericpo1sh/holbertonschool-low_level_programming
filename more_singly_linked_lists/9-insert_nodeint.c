9798b9cd0fc13cca679c858053ae37d5afba16b2
ericpo1sh <ericpo1sh@gmail.com>
2023-07-17 14:13:07 -0700

    test

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -22,7 +22,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (idx == 0)
 	{
 		*head = new;
-		while (head->next != NULL)
+		while ((*head)->next != NULL)
 		(*head) = (*head)->next;
 	}
 
