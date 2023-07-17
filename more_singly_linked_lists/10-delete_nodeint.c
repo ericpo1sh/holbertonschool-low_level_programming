657a23d75544626c13dc35b2cd3cffa534966b7e
ericpo1sh <ericpo1sh@gmail.com>
2023-07-17 12:41:35 -0700

    test

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -9,18 +9,20 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	unsigned int i = 0;
 	listint_t *temp;
+	listint_t *del;
 
 	if (*head == NULL)
 		return (-1);
 
 	temp = *head;
+	del = *temp->next;
 
 	for (i = 0; temp != NULL; i++)
 	{
 		if (i == index)
 		{
 			free(temp);
-			temp = NULL;
+			*head = del;
 		}
 		temp = temp->next;
 	}
