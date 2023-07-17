cf81e3aa2cc3d15480fddc24283d8c4d7d46521f
ericpo1sh <ericpo1sh@gmail.com>
2023-07-17 10:44:06 -0700

    test

---
+++ more_singly_linked_lists/8-sumlistint.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ *
+ */
+int sum_listint(listint_t *head)
+{
+	listint_t *temp;
+	int sum = 0;
+
+	if (head == NULL)
+		return (0);
+
+	while (h)
+	{
+		head = head->next;
+		sum += head;
+	}
+}
+
