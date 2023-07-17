65713bce1209b6b87fc3598d3ff1c41c7391b42b
ericpo1sh <ericpo1sh@gmail.com>
2023-07-17 13:50:12 -0700

    test

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -9,7 +9,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i = 0;
 	listint_t *temp;
-	listint_t *new;
+	listint_t *new = NULL;
 
 	if (*head == NULL)
 		return (-1);
