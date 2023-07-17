7c4d6520de2361d90cb9beda1f158e1b12ca5d00
ericpo1sh <ericpo1sh@gmail.com>
2023-07-17 13:06:18 -0700

    test

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -25,6 +25,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 			del = temp;
 			*head = current;
 			free(del);
+			continue;
 		}
 		if ((i < index) && (temp->next == NULL))
 			return (-1);
