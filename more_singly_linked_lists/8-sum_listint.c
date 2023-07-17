f0b12338dd0278fda27ac87d7c0d1f805401d36d
ericpo1sh <ericpo1sh@gmail.com>
2023-07-17 11:02:40 -0700

    test

--- more_singly_linked_lists/8-sum_listint.c
+++ more_singly_linked_lists/8-sum_listint.c
@@ -7,15 +7,18 @@
  */
 int sum_listint(listint_t *head)
 {
+	listint_t *temp;
 	int sum = 0;
 
 	if (head == NULL)
 		return (0);
+	
+	temp = *head;
 
 	while (head)
 	{
-		head = head->next;
-		sum += (int) *head;
+		sum += temp->n;
+		temp = temp->next;
 	}
 return (sum);
 }
