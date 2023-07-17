474ae4a514f25158e4ead84f8d6a5c4edf111bbf
ericpo1sh <ericpo1sh@gmail.com>
2023-07-17 09:31:33 -0700

    test

--- more_singly_linked_lists/6-pop_listint.c
+++ more_singly_linked_lists/6-pop_listint.c
@@ -1,22 +1,23 @@
 #include "lists.h"
 /**
- *
- *
- *
+ * pop_listint - deletes the head node of the list
+ * @head : the head node
+ * Return: 0 if failed, i if passed.
  *
  */
 int pop_listint(listint_t **head)
 {
 	listint_t *temp;
+	int i = 0;
 
 	if (*head == NULL)
 		return (0);
 	else
 	{
-		*temp = *head;
-		*head = head->next;
-		free(*temp);
-		temp = NULL;
+		temp = *head;
+		i = temp->n;
+		*head = *head->next;
+		free(temp);
 	}
-	return (*head);
+	return (i);
 }
