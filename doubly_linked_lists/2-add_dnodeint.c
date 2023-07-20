2b46c22e919040bc83d3653caf683f42bf91df94
ericpo1sh <ericpo1sh@gmail.com>
2023-07-20 07:10:24 -0700

    Write a function that adds a new node at the beginning of a dlistint_t list.

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * add_dnodeint - adds a new node as head node
+ * @head : head node
+ * @n : new node
+ * Return: NULL if failed, store if succes.
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint *store;
+
+	if (head != NULL)
+	{
+		store = malloc((sizeof(dlistint_t)));
+		if (store == NULL)
+			return (NULL);
+		store->n = n;
+		store-> next = *head;
+		*head = store;
+		return (store);
+	}
+	return (0);
+}
