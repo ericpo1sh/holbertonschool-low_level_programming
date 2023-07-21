35306782325b97612e133dac06b2b7887754d719
ericpo1sh <ericpo1sh@gmail.com>
2023-07-21 16:42:48 -0700

    test

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - gets the value associated with key 
+ * @ht : hash table
+ * @key : key
+ * Return: NULL if failed, value at key if success
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *temp = NULL;
+	unsigned long int index;
+
+	if (ht == NULL || key == NULL)
+		return (NULL);
+
+	index = (hash_djb2((const unsigned char *)key)) % (ht->size);
+
+	if (ht->array[index] == NULL)
+		return (NULL);
+
+	for (temp = ht->array[index]; temp != NULL; temp = temp->next)
+	{
+		if (strmp(temp->key, key) == 0)
+		{
+			return (temp->value);
+		}
+	}
+	return (NULL);
+}
