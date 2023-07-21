2980f59cc02f0f82b6fd53d771fecb53ceba3f73
ericpo1sh <ericpo1sh@gmail.com>
2023-07-20 21:18:28 -0700

    test

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -23,6 +23,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 			(*head)->next->prev = NULL;
 		free(del);
 		*head = (*head)->next;
+		return (1);
 	}
 
 	while (i < index - 1)
