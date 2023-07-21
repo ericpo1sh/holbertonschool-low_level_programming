17caefb0fe58df14fd1d9d72efde4681fd8ada8a
ericpo1sh <ericpo1sh@gmail.com>
2023-07-21 08:34:40 -0700

    test

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -28,6 +28,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			if (*h != NULL)
 				(*h)->prev = new;
 			*h = new;
+			return (new);
 		}
 		if (i + 1 == idx)
 		{
