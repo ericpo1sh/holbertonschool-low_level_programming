8045cce31ad606ec1e41ee458e4b221c56b3637a
ericpo1sh <ericpo1sh@gmail.com>
2023-07-21 12:50:10 -0700

    test

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -9,7 +9,7 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *add = NULL;
-	hash_node_t *headi = NULL;
+	hash_node_t *head = NULL;
 	unsigned long int index;
 
 	add = malloc(sizeof(hash_node_t));
