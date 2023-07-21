02e0c0765fb0a4b9835d5ef5a41899185f86e4d6
ericpo1sh <ericpo1sh@gmail.com>
2023-07-21 15:35:05 -0700

    test

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -30,13 +30,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	else
 	{
 		head = ht->array[index];
-		add->next = NULL;
-		ht->array[index] = add;
-		free(head);
-	}
-	if (ht->array[index] != NULL)
-	{
-		head = ht->array[index];
 		add->next = head;
 		ht->array[index] = add;
 	}
