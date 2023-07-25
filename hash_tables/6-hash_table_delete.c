ae99d3ad776a391076798bc809ccd5560c597dba
ericpo1sh <ericpo1sh@gmail.com>
2023-07-24 20:33:50 -0700

    c

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+/**
+ * hash_table_delete - deletes hash table
+ * @ht : hash table
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *temp, *current;
+
+	if (ht == NULL)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		current = ht->array[i];
+		while (current != NULL)
+		{
+			if (current->value != NULL)
+				free(current->value);
+			if (current->key != NULL)
+				free(current->key);
+			temp = current->next;
+			free(temp);
+			current = temp;
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
