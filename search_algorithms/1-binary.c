73a421c7a85fb60cc7e1d9e9b9c41fc117863070
ericpo1sh <ericpo1sh@gmail.com>
2023-08-17 09:35:53 -0700

    test

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -21,7 +21,7 @@ int binary_search(int *array, size_t size, int value)
 		{
 			if (value == array[mid])
 			{
-				printf("Found %i at index: %d\n", value, mid);
+				printf("Found %i at index: %ld\n", value, mid);
 				return (mid);
 			}
 			else if (value < array[mid])
